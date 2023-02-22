590aed25da5125c185ab348f907275cc4f57c717
pmanonline <petersonzoconis@gmail.com>
2023-02-22 19:14:41 +0100

    pulling ijeoma

--- components/AllCards/testimonialCards.tsx
+++ components/AllCards/testimonialCards.tsx
@@ -6,227 +6,229 @@ import Tstyles from "@/styles/componentstyles/testimonial.module.scss";
 import Image from "next/image";
 import testimonialCard from "../../public/images/testimonial/testimonialimage.png";
 
-export const TestimonialCard1 = () => {
-  return (
-    <main className="">
-      <Card
-        className={`px-4 py-4 mx-auto container border-0 ${styles.ServiceCardBG2} ${Tstyles.TestimonialCardSM}`}
-      >
-        <div className="row">
-          <div className="col-lg-2 col-md-2 col-sm-12 col-xs-12 mt-1">
-            <Image
-              className={`my-3 ${Tstyles.TcardImage}`}
-              src={testimonialCard}
-              alt="testimonial image"
-            />
-          </div>
-
-          <div
-            className={`col-lg-10 col-md-10 col-sm-12 col-xs-12   ${Tstyles.imgspacing}`}
-          >
-            <span className={`${Tstyles.testimonialName}`}>
-              Esther blessing
-            </span>
-            <br />
-            <span className={`${Tstyles.testimonialPosition}`}>
-              "CEO, Dara Wears
-            </span>
-          </div>
-        </div>
-        Lorem ipsum dolor sit amet consectetur. Platea vel feugiat aenean arcu
-        purus egestas. Pulvinar nunc sed sed vulputate tortor tristique
-        condimentum augue at. Ullamcorper facilisis ut pellentesque nunc. Eget
-        eget at quam sit proin enim tincidunt.
-      </Card>
-    </main>
-  );
-};
-
-export const TestimonialCard2 = () => {
-  return (
-    <main className="">
-      <Card
-        className={`px-4 py-4 mx-auto container  border-0 ${styles.ServiceCardBG} ${Tstyles.TestimonialCardSM}`}
-      >
-        <div className="row">
-          <div className="col-lg-2 col-md-2 col-sm-12 col-xs-12 mt-1">
-            <Image
-              className={`my-3 ${Tstyles.TcardImage}`}
-              src={testimonialCard}
-              alt="testimonial image"
-            />
-          </div>
-
-          <div
-            className={`col-lg-10 col-md-10 col-sm-12 col-xs-12   ${Tstyles.imgspacing}`}
-          >
-            <span className={`${Tstyles.testimonialName}`}>
-              Esther blessing
-            </span>
-            <br />
-            <span className={`${Tstyles.testimonialPosition}`}>
-              "CEO, Dara Wears
-            </span>
-          </div>
-        </div>
-        Lorem ipsum dolor sit amet consectetur. Platea vel feugiat aenean arcu
-        purus egestas. Pulvinar nunc sed sed vulputate tortor tristique
-        condimentum augue at. Ullamcorper facilisis ut pellentesque nunc. Eget
-        eget at quam sit proin enim tincidunt.
-      </Card>
-    </main>
-  );
-};
-
-export const TestimonialCard3 = () => {
-  return (
-    <main className="">
-      <Card
-        className={`px-4 py-4 mx-auto container  border-0 ${styles.ServiceCardBG} ${Tstyles.TestimonialCardSM}`}
-      >
-        <div className="row">
-          <div className="col-lg-2 col-md-2 col-sm-12 col-xs-12 mt-1">
-            <Image
-              className={`my-3 ${Tstyles.TcardImage}`}
-              src={testimonialCard}
-              alt="testimonial image"
-            />
-          </div>
-          <div
-            className={`col-lg-10 col-md-10 col-sm-12 col-xs-12   ${Tstyles.imgspacing}`}
-          >
-            <span className={`${Tstyles.testimonialName}`}>
-              Esther blessing
-            </span>
-            <br />
-            <span className={`${Tstyles.testimonialPosition}`}>
-              "CEO, Dara Wears
-            </span>
-          </div>
-        </div>
-        Lorem ipsum dolor sit amet consectetur. Platea vel feugiat aenean arcu
-        purus egestas. Pulvinar nunc sed sed vulputate tortor tristique
-        condimentum augue at. Ullamcorper facilisis ut pellentesque nunc. Eget
-        eget at quam sit proin enim tincidunt.
-      </Card>
-    </main>
-  );
-};
-
-export const TestimonialCard4 = () => {
-  return (
-    <main className="">
-      <Card
-        className={`px-4 py-4 mx-auto container  border-0 ${styles.ServiceCardBG2} ${Tstyles.TestimonialCardSM}`}
-      >
-        <div className="row">
-          <div className="col-lg-2 col-md-2 col-sm-12 col-xs-12 mt-1">
-            <Image
-              className={`my-3 ${Tstyles.TcardImage}`}
-              src={testimonialCard}
-              alt="testimonial image"
-            />
-          </div>
-
-          <div
-            className={`col-lg-10 col-md-10 col-sm-12 col-xs-12   ${Tstyles.imgspacing}`}
-          >
-            <span className={`${Tstyles.testimonialName}`}>
-              Esther blessing
-            </span>
-            <br />
-            <span className={`${Tstyles.testimonialPosition}`}>
-              "CEO, Dara Wears
-            </span>
-          </div>
-        </div>
-        Lorem ipsum dolor sit amet consectetur. Platea vel feugiat aenean arcu
-        purus egestas. Pulvinar nunc sed sed vulputate tortor tristique
-        condimentum augue at. Ullamcorper facilisis ut pellentesque nunc. Eget
-        eget at quam sit proin enim tincidunt.
-      </Card>
-    </main>
-  );
-};
-export const SingleTestimonialCard = () => {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-6 col-md-6 col-sm-12 col- xs-12 my-3 ">
-          <TestimonialCard1 />
-        </div>
-        <div className="col-lg-6 col-md-6 col-sm-12 col- xs-12 my-3 ">
-          {" "}
-          <TestimonialCard2 />
-        </div>
-        <div className="col-lg-6 col-md-6 col-sm-12 col- xs-12 my-3 ">
-          {" "}
-          <TestimonialCard3 />
-        </div>
-        <div className="col-lg-6 col-md-6 col-sm-12 col- xs-12 my-3 ">
-          {" "}
-          <TestimonialCard4 />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-// interface ITestimonial {
-//   Id: number;
-//   Images: string;
-//   Name: string;
-//   Position: string;
-//   text: string;
-// }
-
-// export const TestimonialCard = (props: ITestimonial) => {
-//   const { Images, Name, Position, text } = props;
+// export const TestimonialCard1 = () => {
 //   return (
 //     <main className="">
 //       <Card
-//         className={`px-4 py-4 mx-auto container ${styles.ServiceCardBG} ${Tstyles.TestimonialCardSM}`}
+//         className={`px-4 py-4 mx-auto container border-0 ${styles.ServiceCardBG2} ${Tstyles.TestimonialCardSM}`}
 //       >
 //         <div className="row">
-//           <div className="col-lg-2 col-md-2 col-sm-12 col-xs-12">
-//             <Card.Img
-//               className={`ms-2 mt-3 ${Tstyles.TcardImage}`}
-//               src={Images}
-//               style={{ width: "3rem" }}
+//           <div className="col-lg-2 col-md-2 col-sm-12 col-xs-12 mt-1">
+//             <Image
+//               className={`my-3 ${Tstyles.TcardImage}`}
+//               src={testimonialCard}
+//               alt="testimonial image"
 //             />
 //           </div>
 
-//           <div className="col-lg-10 col-md-10 col-sm-12 col-xs-12 mt-3">
-//             <span className={`${Tstyles.testimonialName}`}>{Name}</span>
+//           <div
+//             className={`col-lg-10 col-md-10 col-sm-12 col-xs-12   ${Tstyles.imgspacing}`}
+//           >
+//             <span className={`${Tstyles.testimonialName}`}>
+//               Esther blessing
+//             </span>
 //             <br />
-//             <span className={`${Tstyles.testimonialPosition}`}>{Position}</span>
+//             <span className={`${Tstyles.testimonialPosition}`}>
+//               "CEO, Dara Wears
+//             </span>
 //           </div>
 //         </div>
-//         {text}
+//         Lorem ipsum dolor sit amet consectetur. Platea vel feugiat aenean arcu
+//         purus egestas. Pulvinar nunc sed sed vulputate tortor tristique
+//         condimentum augue at. Ullamcorper facilisis ut pellentesque nunc. Eget
+//         eget at quam sit proin enim tincidunt.
 //       </Card>
 //     </main>
 //   );
 // };
 
+// export const TestimonialCard2 = () => {
+//   return (
+//     <main className="">
+//       <Card
+//         className={`px-4 py-4 mx-auto container  border-0 ${styles.ServiceCardBG} ${Tstyles.TestimonialCardSM}`}
+//       >
+//         <div className="row">
+//           <div className="col-lg-2 col-md-2 col-sm-12 col-xs-12 mt-1">
+//             <Image
+//               className={`my-3 ${Tstyles.TcardImage}`}
+//               src={testimonialCard}
+//               alt="testimonial image"
+//             />
+//           </div>
+
+//           <div
+//             className={`col-lg-10 col-md-10 col-sm-12 col-xs-12   ${Tstyles.imgspacing}`}
+//           >
+//             <span className={`${Tstyles.testimonialName}`}>
+//               Esther blessing
+//             </span>
+//             <br />
+//             <span className={`${Tstyles.testimonialPosition}`}>
+//               "CEO, Dara Wears
+//             </span>
+//           </div>
+//         </div>
+//         Lorem ipsum dolor sit amet consectetur. Platea vel feugiat aenean arcu
+//         purus egestas. Pulvinar nunc sed sed vulputate tortor tristique
+//         condimentum augue at. Ullamcorper facilisis ut pellentesque nunc. Eget
+//         eget at quam sit proin enim tincidunt.
+//       </Card>
+//     </main>
+//   );
+// };
+
+// export const TestimonialCard3 = () => {
+//   return (
+//     <main className="">
+//       <Card
+//         className={`px-4 py-4 mx-auto container  border-0 ${styles.ServiceCardBG} ${Tstyles.TestimonialCardSM}`}
+//       >
+//         <div className="row">
+//           <div className="col-lg-2 col-md-2 col-sm-12 col-xs-12 mt-1">
+//             <Image
+//               className={`my-3 ${Tstyles.TcardImage}`}
+//               src={testimonialCard}
+//               alt="testimonial image"
+//             />
+//           </div>
+//           <div
+//             className={`col-lg-10 col-md-10 col-sm-12 col-xs-12   ${Tstyles.imgspacing}`}
+//           >
+//             <span className={`${Tstyles.testimonialName}`}>
+//               Esther blessing
+//             </span>
+//             <br />
+//             <span className={`${Tstyles.testimonialPosition}`}>
+//               "CEO, Dara Wears
+//             </span>
+//           </div>
+//         </div>
+//         Lorem ipsum dolor sit amet consectetur. Platea vel feugiat aenean arcu
+//         purus egestas. Pulvinar nunc sed sed vulputate tortor tristique
+//         condimentum augue at. Ullamcorper facilisis ut pellentesque nunc. Eget
+//         eget at quam sit proin enim tincidunt.
+//       </Card>
+//     </main>
+//   );
+// };
+
+// export const TestimonialCard4 = () => {
+//   return (
+//     <main className="">
+//       <Card
+//         className={`px-4 py-4 mx-auto container  border-0 ${styles.ServiceCardBG2} ${Tstyles.TestimonialCardSM}`}
+//       >
+//         <div className="row">
+//           <div className="col-lg-2 col-md-2 col-sm-12 col-xs-12 mt-1">
+//             <Image
+//               className={`my-3 ${Tstyles.TcardImage}`}
+//               src={testimonialCard}
+//               alt="testimonial image"
+//             />
+//           </div>
+
+//           <div
+//             className={`col-lg-10 col-md-10 col-sm-12 col-xs-12   ${Tstyles.imgspacing}`}
+//           >
+//             <span className={`${Tstyles.testimonialName}`}>
+//               Esther blessing
+//             </span>
+//             <br />
+//             <span className={`${Tstyles.testimonialPosition}`}>
+//               "CEO, Dara Wears
+//             </span>
+//           </div>
+//         </div>
+//         Lorem ipsum dolor sit amet consectetur. Platea vel feugiat aenean arcu
+//         purus egestas. Pulvinar nunc sed sed vulputate tortor tristique
+//         condimentum augue at. Ullamcorper facilisis ut pellentesque nunc. Eget
+//         eget at quam sit proin enim tincidunt.
+//       </Card>
+//     </main>
+//   );
+// };
 // export const SingleTestimonialCard = () => {
 //   return (
 //     <div className="container">
 //       <div className="row">
-//         {testimonial.testimonialDetails.map((items) => {
-//           return (
-//             <>
-//               <div
-//                 className="col-lg-6 col-md-6 col-sm-12 col- xs-12 my-3 "
-//                 key={items.Id}
-//               >
-//                 <TestimonialCard {...items} />
-//               </div>
-//             </>
-//           );
-//         })}
+//         <div className="col-lg-6 col-md-6 col-sm-12 col- xs-12 my-3 ">
+//           <TestimonialCard1 />
+//         </div>
+//         <div className="col-lg-6 col-md-6 col-sm-12 col- xs-12 my-3 ">
+//           {" "}
+//           <TestimonialCard2 />
+//         </div>
+//         <div className="col-lg-6 col-md-6 col-sm-12 col- xs-12 my-3 ">
+//           {" "}
+//           <TestimonialCard3 />
+//         </div>
+//         <div className="col-lg-6 col-md-6 col-sm-12 col- xs-12 my-3 ">
+//           {" "}
+//           <TestimonialCard4 />
+//         </div>
 //       </div>
 //     </div>
 //   );
 // };
 
-//  className = "col-lg-6 col-md-6 col-sm-12 col- xs-12 my-3 ";
+interface ITestimonial {
+  Id: number;
+  Images: string;
+  Name: string;
+  Position: string;
+  text: string;
+  rgba: string;
+}
+
+export const TestimonialCard = (props: ITestimonial) => {
+  const { Images, Name, Position, text, rgba } = props;
+  return (
+    <main className="">
+      <Card
+        style={{
+          backgroundColor: `${rgba}`,
+        }}
+        className={`px-4 py-4 mx-auto container ${styles.ServiceCardBG} ${Tstyles.TestimonialCardSM}`}
+      >
+        <div className="row">
+          <div className="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+            <Card.Img
+              className={`ms-2 mt-3 ${Tstyles.TcardImage}`}
+              src={Images}
+              style={{ width: "3rem" }}
+            />
+          </div>
+
+          <div className="col-lg-10 col-md-10 col-sm-12 col-xs-12 mt-3">
+            <span className={`${Tstyles.testimonialName}`}>{Name}</span>
+            <br />
+            <span className={`${Tstyles.testimonialPosition}`}>{Position}</span>
+          </div>
+        </div>
+        {text}
+      </Card>
+    </main>
+  );
+};
+
+export const SingleTestimonialCard = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        {testimonial.testimonialDetails.map((items) => {
+          return (
+            <>
+              <div
+                className="col-lg-6 col-md-6 col-sm-12 col- xs-12 my-3 "
+                key={items.Id}
+              >
+                <TestimonialCard {...items} />
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
--- components/contactUs.tsx
+++ components/contactUs.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import contactStyle from "@/styles/componentstyles/contactUs.module.scss";
 import styles from "@/styles/generalstyles/colors.module.scss";
 import Form from "react-bootstrap/Form";
@@ -9,7 +10,8 @@ export const ContactUs = () => {
   return (
     <>
       <div
-        className={`container mx-auto ${styles.pryColor}  ${contactStyle.contactSection}`}>
+        className={`container mx-auto ${styles.pryColor}  ${contactStyle.contactSection}`}
+      >
         <div className="row">
           <div className="col-lg-5 col-md-5 col-sm-12 d-flex justify-content-center align-self-center">
             <h3 className={` ${styles.secColor}`}>Get in touch</h3>
@@ -23,53 +25,212 @@ export const ContactUs = () => {
   );
 };
 
+// const ContactForm = () => {
+//   return (
+//     <>
+//       <div className="col-lg-7 col-md-7 col-sm-12 mx-auto">
+//         <Form className={`bg-white p-3 ${contactStyle.formstyle}`}>
+//           <div className={`mb-3`}>
+//             <p className={``}>Name:</p>
+//             <input
+//               type="email"
+//               className={`form-control ${contactStyle.inputstyle}`}
+//               id="exampleInputEmail1"
+//             />
+//           </div>
+//           <Form.Group className="mb-3">
+//             <p>Email:</p>
+//             <input
+//               type="email"
+//               className={`form-control ${contactStyle.inputstyle}`}
+//               id="exampleInputEmail1"
+//             />
+//           </Form.Group>
+
+//           <Form.Group className="mb-3">
+//             <Form.Label htmlFor="disabledSelect">Pricing Option</Form.Label>
+//             <Form.Select
+//               id="disabledSelect"
+//               className={`form-control ${contactStyle.inputstyle}`}>
+//               <option value="1">Classic</option>
+//               <option value="2">Advanced</option>
+//               <option value="3">Premium</option>
+//             </Form.Select>
+//           </Form.Group>
+
+//           <Form.Group className="mb-3">
+//             <p>Message:</p>
+//             <textarea
+//               className="form-control"
+//               id="exampleFormControlTextarea1"
+//               placeholder="Message"
+//               rows={3}></textarea>
+//           </Form.Group>
+//           <div className="d-flex justify-content-center">
+//             {/* <Button type="submit">submit</Button> */}
+//             <SubmitButton />
+//           </div>
+//         </Form>
+//       </div>
+//     </>
+//   );
+// };
+
+interface Product {
+  Name: string;
+  Email: string;
+  PricingOption: string;
+  Message: string;
+}
+
 const ContactForm = () => {
+  const [person, setPerson] = useState({
+    Name: "",
+    Email: "",
+    PricingOption: "",
+    Message: "",
+  });
+  const [people, setPeople] = useState([]);
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setPerson({ ...person, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (person.Name && person.Email && person.PricingOption && person.Message) {
+      const newPerson = { ...person };
+      setPeople([...people, newPerson]);
+      setPerson({ Name: "", Email: "", PricingOption: "", Message: "" });
+    }
+  };
   return (
     <>
-      <div className="col-lg-7 col-md-7 col-sm-12 mx-auto">
-        <Form className={`bg-white p-3 ${contactStyle.formstyle}`}>
-          <div className={`mb-3`}>
-            <p className={``}>Name:</p>
+      <article className="form">
+        <form>
+          <div className="form-control">
+            <label htmlFor="firstName">Name : </label>
             <input
-              type="email"
-              className={`form-control ${contactStyle.inputstyle}`}
-              id="exampleInputEmail1"
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={person.Name}
+              onChange={handleChange}
             />
           </div>
-          <Form.Group className="mb-3">
-            <p>Email:</p>
+          <div className="form-control">
+            <label htmlFor="email">Email : </label>
             <input
               type="email"
-              className={`form-control ${contactStyle.inputstyle}`}
-              id="exampleInputEmail1"
+              id="email"
+              name="email"
+              value={person.Email}
+              onChange={handleChange}
             />
-          </Form.Group>
-
-          <Form.Group className="mb-3">
-            <Form.Label htmlFor="disabledSelect">Pricing Option</Form.Label>
-            <Form.Select
-              id="disabledSelect"
-              className={`form-control ${contactStyle.inputstyle}`}>
-              <option value="1">Classic</option>
-              <option value="2">Advanced</option>
-              <option value="3">Premium</option>
-            </Form.Select>
-          </Form.Group>
-
-          <Form.Group className="mb-3">
-            <p>Message:</p>
-            <textarea
-              className="form-control"
-              id="exampleFormControlTextarea1"
-              placeholder="Message"
-              rows={3}></textarea>
-          </Form.Group>
-          <div className="d-flex justify-content-center">
-            {/* <Button type="submit">submit</Button> */}
-            <SubmitButton />
           </div>
-        </Form>
-      </div>
+          <div className="form-control">
+            <label htmlFor="age">Age : </label>
+            <input
+              type="number"
+              id="age"
+              name="age"
+              value={person.PricingOption}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit" className="btn" onClick={handleSubmit}>
+            add person
+          </button>
+        </form>
+      </article>
+      <article>
+        {people.map((person) => {
+          const { id, firstName, email, age } = person;
+          return (
+            <div key={id} className="item">
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+              <p>{age}</p>
+            </div>
+          );
+        })}
+      </article>
     </>
   );
 };
+
+// interface Product {
+//   Name: string;
+//   Email: string;
+//   PricingOption: string;
+//   Message: string;
+// }
+
+// const ContactForm = (props: Product) => {
+//   const [Name, setName] = useState("");
+//   const [Email, setEmail] = useState("");
+//   const [PricingOption, setPricingOption] = useState("");
+//   const [Message, setMessage] = useState("");
+//   const [people, setPeople] = useState([]);
+
+//   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     if (Name && Email && PricingOption && Message) {
+//       const person = {
+//         id: new Date().getTime().toString(),
+//         Name,
+//         Email,
+//         PricingOption,
+//         Message,
+//       };
+//       console.log(person);
+//       setPeople((people) => {
+//         return [...people, person];
+//       });
+//       setName("");
+//       setEmail("");
+//       setPricingOption("");
+//       setMessage("");
+//     } else {
+//       console.log("empty values");
+//     }
+//   };
+//   return (
+//     <>
+//       <article>
+//         <form className="form" onSubmit={handleSubmit}>
+//           <div className="form-control">
+//             <label htmlFor="firstName">Name : </label>
+//             <input
+//               type="text"
+//               id="firstName"
+//               name="firstName"
+//               value={firstName}
+//               onChange={(e) => setFirstName(e.target.value)}
+//             />
+//           </div>
+//           <div className="form-control">
+//             <label htmlFor="email">Email : </label>
+//             <input
+//               type="email"
+//               id="email"
+//               name="email"
+//               value={email}
+//               onChange={(e) => setEmail(e.target.value)}
+//             />
+//           </div>
+//           <button type="submit">add person</button>
+//         </form>
+//         {people.map((person, index) => {
+//           const { id, firstName, email } = person;
+//           return (
+//             <div className="item" key={id}>
+//               <h4>{firstName}</h4>
+//               <p>{email}</p>
+//             </div>
+//           );
+//         })}
+//       </article>
+//     </>
+//   );
+// };
